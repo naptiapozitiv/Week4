ab3fa76045094256839e9912916c302a2f27fbf5
levanchuk2002@outlook.com <levanchuk2002@outlook.com>
2023-11-08 20:50:10 -0500

    Created a class and make a hw

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,37 +1,11 @@
-﻿int input = 0;
-string variable = " ";
-Console.WriteLine("Enter number between 1 and 1000:");
-input = Console.Read();
-if (1 >= input || input >= 1000)
-    Console.WriteLine("Wrong input");
-else
-{
-    variable = Fibonacci(input);
-    Console.WriteLine(variable);
-}
-static string Fibonacci(int input)
-{
-    string result = "";
-    int number1 = 0;
-    int number2 = 1;
-    int number3 = 0;
+﻿using ConsoleApp1;
 
-    while (number3 < input)
-    {
-        if (number3 < input)
-        {
-            number3 = number1 + number2;
-            if (number3 <= input)
-            {
-                result += number3 + ";";
-            }
-            number1 = number2;
-            number2 = number3;
-        }
-        else
-        {
-            continue;
-        }
-    }
-    return result;
-}
+Console.WriteLine("Enter text:");
+string input = Console.ReadLine();
+var text = new TextHelper(input);
+var wordsCounter = text.CountWordWith4orMoreWords(input);
+var poli = text.IsPoli(input);
+var VowelsandCon = text.CountLetterC_and_V(input);
+Console.WriteLine($"words with more than 4 letters: {wordsCounter}");
+Console.WriteLine($"Its Palindrom:{poli}");
+Console.WriteLine(VowelsandCon);
---
+++ ConsoleApp1/TextHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class TextHelper
+    {
+        public string text;
+        public TextHelper(string t)
+        {
+            text = t;
+        }
+        public int CountWordWith4orMoreWords(string text)
+        {
+            int counter = 0;
+            string[] word = text.Split(' ');
+            foreach (string words in word)
+            {
+                if (word.Length <= 4)
+                    counter++;
+            }
+            return counter;
+        }
+        public bool IsPoli(string text)
+        {
+            text.Replace(" ", "").ToLower();
+
+            for (int i = 0; i < text.Length / 2; i++)
+            {
+                if (text[i] != text[text.Length -1 - i])
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+        public string CountLetterC_and_V(string text)
+        {
+            string result;
+            int VowelCounter = 0;
+            var ignore = @"[?.,;:'!-()]";
+            text.Replace(" ", "").Replace(ignore, "").ToLower();
+            int ConsonanceCounter = 0;
+            foreach (char character in text)
+            {
+                if (!Char.IsLetter(character))
+                    continue;
+                if (character == 'a' || character == 'e' || character == 'i' || character == 'y' || character == 'o' || character == 'u')
+                    VowelCounter++;
+                else
+                    ConsonanceCounter++;
+            }
+            int sum = ConsonanceCounter + VowelCounter;
+            result = $"Number of Vowels is {VowelCounter}, Number of Consonances is {ConsonanceCounter}, sum of characters is {sum}";
+            return result;
+        }
+    }
+}
